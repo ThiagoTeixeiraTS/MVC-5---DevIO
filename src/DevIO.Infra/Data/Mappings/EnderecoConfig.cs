f981f3b0babbba813703d5a18d1cd4095275244d
Thiago José Teixeira de Araujo <thiago.araujo@sigcorp.com.br>
2021-06-20 21:21:52 -0300

    Commit - Modelagem Via Context

---
+++ src/DevIO.Infra/Data/Mappings/EnderecoConfig.cs
@@ -0,0 +1,51 @@
+﻿using DevIO.Business.Models.Fornecedores;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevIO.Infra.Data.Mappings
+{
+    public class EnderecoConfig : EntityTypeConfiguration<Endereco>
+    {
+        public EnderecoConfig()
+        {
+            HasKey(p => p.Id);
+
+            Property(p => p.Logradouro)
+                .IsRequired()
+                .HasMaxLength(200);
+
+            Property(f => f.Numero)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            Property(f => f.Cep)
+                .IsRequired()
+                .HasMaxLength(8)
+            .IsFixedLength();
+
+            Property(f => f.Complemento)
+                    .HasMaxLength(250);
+
+
+            Property(p => p.Cidade)
+                .IsRequired()
+                .HasMaxLength(200);
+
+
+            Property(p => p.Estado)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            ToTable("Enderecos");
+
+
+
+
+
+        }
+    }
+}
---
+++ src/DevIO.Infra/Data/Mappings/FornecedorConfig.cs
@@ -0,0 +1,34 @@
+﻿using DevIO.Business.Models.Fornecedores;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure.Annotations;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevIO.Infra.Data.Mappings
+{
+    public class FornecedorConfig : EntityTypeConfiguration<Fornecedor>
+    {
+        public FornecedorConfig()
+        {
+            HasKey(f => f.Id);
+            Property(f => f.Nome)
+                .IsRequired()
+                .HasMaxLength(200);
+
+            Property(f => f.Documento)
+                .IsRequired()
+                .HasMaxLength(14)
+                .HasColumnAnnotation(name: "index",
+                value: new IndexAnnotation(new System.ComponentModel.DataAnnotations.Schema.IndexAttribute { IsUnique = true }));
+
+
+            HasRequired(f => f.Endereco)
+                .WithRequiredPrincipal(f => f.Fornecedor);
+
+            ToTable("Fornecedores");
+    }
+    }
+}
