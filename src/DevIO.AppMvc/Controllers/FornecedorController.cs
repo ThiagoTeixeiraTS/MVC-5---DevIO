d311e75e082411758be0fbece8f3481483b6940e
Thiago José Teixeira de Araujo <thiago.araujo@sigcorp.com.br>
2021-06-28 21:59:32 -0300

    Commit - Services e Notificador

---
+++ src/DevIO.AppMvc/Controllers/FornecedorController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DevIO.AppMvc.Controllers
+{
+    public class FornecedorController : Controller
+    {
+        // GET: Fornecedor
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
